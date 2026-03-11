e17f1a1f5f742cf457f965451e76f158d28e5b75
Aya D <dzhaicholpon@gmail.com>
2026-03-11 09:40:43 -0500

    fix: force add all required files

--- app.js
+++ app.js
@@ -107,5 +107,5 @@ app.use((err, req, res, next) => {
   res.status(500).redirect("/");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
